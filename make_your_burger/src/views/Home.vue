482ce8219c7bd87a1d4e0874f5bb3552de7d5775
mateusnerigo <mateuslimaneri@gmail.com>
2022-07-24 23:33:49 -0300

    #09 - classes 23 to 24/34

--- make_your_burger/src/views/Home.vue
+++ make_your_burger/src/views/Home.vue
@@ -1,9 +1,20 @@
 <template>
-  <div class="main-container">
-    <h1>Home</h1>
+  <div>
+    <Banner />
+    <div class="main-container">
+      <h1>Monte o seu burger: </h1>
+      <BurgerForm />
+    </div>
   </div>
 </template>
 
 <script>
-
+  import Banner from '../components/Banner.vue';
+  import BurgerForm from '../components/BurgerForm.vue';
+  export default {
+    components: {
+      Banner,
+      BurgerForm
+    }
+  }
 </script>
